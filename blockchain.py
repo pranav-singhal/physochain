a14f309343a2c6105c80dba7556bd92c2163a88b
pranav singhal <pranavsinghal96@gmail.com>
2017-11-10 19:40:51 +0530

    commented flask code

--- blockchain.py
+++ blockchain.py
@@ -57,49 +57,48 @@ class Blockchain(object):
         guess_hash=hashlib.sha256(guess).hexdigest()
         return guess_hash[:4]=='0000'
 
-app = Flask(__name__)
-node_identifier = str(uuid4()).replace('-', '')
+# app = Flask(__name__)
+# node_identifier = str(uuid4()).replace('-', '')
 
 blockchain = Blockchain()
 
-# print(blockchain.chain[-1])
-@app.route('/transactions/new',methods=['POST'])
-def new_transaction():
-    values=request.get_json()
-    print(values)
-    required = ['sender','recipient','amount']
-    if not all(k in values for k in required):
-        return 'missing values', 400
-    index=blockchain.new_transaction(values['sender'],values['recipient'],values['amount'])
-    response = {'message':f'Transaction will be added to block{index}'}
-    return jsonify(response),201
-
-
-@app.route('/mine', methods=['GET'])
-def mine():
-    last_block=blockchain.last_block
-    last_proof=last_block['proof']
-    proof=blockchain.proof_of_work(last_proof)
-    blockchain.new_transaction(sender="0",recipient=node_identifier,amount=1)
-    block=blockchain.new_block(proof)
-    response = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
-    }
-    return jsonify(response), 200
-
-@app.route('/chain', methods=['GET'])
-def full_chain():
-    response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain),
-    }
-    return jsonify(response), 200
-
-app.run(host='0.0.0.0', port=5000)
+# @app.route('/transactions/new',methods=['POST'])
+# def new_transaction():
+#     values=request.get_json()
+#     print(values)
+#     required = ['sender','recipient','amount']
+#     if not all(k in values for k in required):
+#         return 'missing values', 400
+#     index=blockchain.new_transaction(values['sender'],values['recipient'],values['amount'])
+#     response = {'message':f'Transaction will be added to block{index}'}
+#     return jsonify(response),201
+#
+#
+# @app.route('/mine', methods=['GET'])
+# def mine():
+#     last_block=blockchain.last_block
+#     last_proof=last_block['proof']
+#     proof=blockchain.proof_of_work(last_proof)
+#     blockchain.new_transaction(sender="0",recipient=node_identifier,amount=1)
+#     block=blockchain.new_block(proof)
+#     response = {
+#         'message': "New Block Forged",
+#         'index': block['index'],
+#         'transactions': block['transactions'],
+#         'proof': block['proof'],
+#         'previous_hash': block['previous_hash'],
+#     }
+#     return jsonify(response), 200
+#
+# @app.route('/chain', methods=['GET'])
+# def full_chain():
+#     response = {
+#         'chain': blockchain.chain,
+#         'length': len(blockchain.chain),
+#     }
+#     return jsonify(response), 200
+#
+# app.run(host='0.0.0.0', port=5000)
 
     # json.dumps() => takes a json and converts it into a string
     # @ => used for static methods
